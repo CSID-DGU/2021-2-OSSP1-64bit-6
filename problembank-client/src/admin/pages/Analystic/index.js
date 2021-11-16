d0d6221fd504479baec85f239df2fd628e98f3eb
KwanJung98 <remf123@gmail.com>
2021-11-16 14:54:17 +0900

    관리자 대시보드 - 데이터 강제 갱신 기능 추가

--- problembank-client/src/admin/pages/Analystic/index.js
+++ problembank-client/src/admin/pages/Analystic/index.js
@@ -9,12 +9,29 @@ import ProblemNumCard from '../../components/ProblemNumCard';
 import UserGraphCard from '../../components/UserGraphCard';
 import ProblemGraphCard from '../../components/ProblemGraphCard';
 import UserRankCard from '../../components/UserRankCard';
+import { HiOutlineRefresh} from "react-icons/hi";
 
 function Analystic(props) {
+    const [timeState,setTime] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let present_date = new Date();
+            let time_string =  present_date.getFullYear() + '/'+ ('0' + (present_date.getMonth() + 1)).slice(-2) +'/' + ('0' + present_date.getDate()).slice(-2) + " "
+                             + ('0'+present_date.getHours()).slice(-2) + ":"+('0'+present_date.getMinutes()).slice(-2);
+            setTime(time_string);
+        };
+        fetchData();
+    }, []);
+
+
 	return (
 		<DashboardLayout>
-            <h1 className="Title_dashboard">DashBoard</h1>
-
+            <div className="Header">
+                <h1 className="Title_dashboard">DashBoard </h1>
+                <div className="Refresh_Icon" onClick={() => window.location.reload()}><HiOutlineRefresh size="20" color ="grey"/></div>
+                <div className="Refresh_Time">{timeState}</div>
+            </div>
             <div className="Full_container">
                 <div className="User_Num_Card"> <UserNumCard/></div>
                 <div className="Problem_Num_Card"> <ProblemNumCard/></div>
